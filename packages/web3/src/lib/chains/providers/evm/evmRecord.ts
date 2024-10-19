0c1ed340abb81b3142ec7d4ff6f7e193936ce487
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-19 09:49:09 +0330

    bug fix + enhance record process + add more modalInterface logs

--- packages/web3/src/lib/chains/providers/evm/evmRecord.ts
+++ packages/web3/src/lib/chains/providers/evm/evmRecord.ts
@@ -2,20 +2,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ethers } from 'ethers';
 import {
-  Beneficiary,
-  EthAddress,
   NFTType,
   PaymentMethodType,
-  ProductType,
   RecordResponse,
 } from '../../dto/constants/chain-structs';
 import { Unauthorized } from '../../dto/errors/chain-errors';
 import { ContractType, getGasPrice } from '../../dto/constants/chain-constants';
-import { ModalInterface } from '../../dto/interfaces/modal-interface.interface';
 import { RecordProduct } from '../../dto/record.dto';
-import { AxiosInstance } from 'axios';
 import { getShopABI } from '../../dto/constants/chain-abis';
-import { uploadMetadata } from './evm.helpers';
+import { checkWallet, uploadMetadata } from './evm.helpers';
 import { DroplinkedChainConfig } from '../../dto/configs/chain.config';
 import {
   IProductDetails,
@@ -26,7 +21,8 @@ import { IWeb3Context } from '../../dto/interfaces/web3-context.interface';
 function getRecordData(
   chainConfig: DroplinkedChainConfig,
   product: ISKUDetails & IProductDetails,
-  metadataURL: string
+  metadataURL: string,
+  nftContract: string
 ) {
   const { contractType } = chainConfig;
   const {
@@ -34,25 +30,10 @@ function getRecordData(
     amount,
     beneficiaries,
     currencyAddress,
-    nftContract,
     price,
     royalty,
     type,
   } = product;
-  type Product = {
-    _nftAddress: string;
-    _uri: string;
-    _amount: number;
-    _accepted: boolean;
-    _affiliatePercentage: number;
-    _price: number;
-    _currencyAddress: string;
-    _royalty: number;
-    _nftType: NFTType;
-    _productType: ProductType;
-    _paymentType: PaymentMethodType;
-    _beneficiaries: Beneficiary[];
-  };
   const result = {
     _nftAddress: nftContract,
     _uri: metadataURL,
@@ -80,23 +61,37 @@ export async function recordProduct(
   productData: IProductDetails,
   context: IWeb3Context
 ): Promise<RecordResponse> {
-  const { description, shopAddress } = productData;
-  const { skuID } = skuData;
-  const recordData = { ...skuData, ...productData };
-  console.log(JSON.stringify(recordData));
-  const signer = chainConfig.provider.getSigner();
-  if (
-    (await signer.getAddress()).toLocaleLowerCase() !==
-    chainConfig.address.toLocaleLowerCase()
-  ) {
-    throw new Error('Address does not match signer address');
+  const { nftContract, shopContractAddress, modalInterface } = context;
+
+  if (!nftContract || !shopContractAddress) {
+    throw new Unauthorized(
+      `Missing required fields: nftContract or shopContractAddress`,
+      chainConfig.address,
+      chainConfig.address
+    );
   }
 
+  const { description } = productData;
+  const { skuID } = skuData;
+  const recordData = { ...skuData, ...productData };
+
+  console.log(JSON.stringify(recordData));
+
+  const signer = chainConfig.provider.getSigner();
+
+  modalInterface.waiting('Connecting to wallet...');
+
+  await checkWallet(signer, chainConfig.address);
+
+  modalInterface.waiting('Getting shop contract...');
+
   const contract = new ethers.Contract(
-    shopAddress,
+    shopContractAddress,
     getShopABI(chainConfig.contractType),
     signer
   );
+
+  modalInterface.waiting('Uploading metadata...');
 
   const properties = {
     _id: recordData.skuProperties['_id'],
@@ -119,34 +114,66 @@ export async function recordProduct(
     skuID,
     context.axiosInstance
   );
-  const product = getRecordData(chainConfig, recordData, metadataURL);
+
+  modalInterface.waiting(`Metadata uploaded: ${metadataURL}`);
+
+  modalInterface.waiting('Getting recordData');
+  const product = getRecordData(
+    chainConfig,
+    recordData,
+    metadataURL,
+    nftContract
+  );
+
+  modalInterface.waiting('Got recordData');
 
   try {
-    await contract.callStatic['mintAndRegister'](product);
-    const gasEstimation = (
-      await contract.estimateGas['mintAndRegister'](product)
-    ).toBigInt();
-
-    const gasPrice = (await getGasPrice(chainConfig.provider)).valueOf();
-    const tx = await contract['mintAndRegister'](product, {
-      gasLimit: (gasEstimation * BigInt(105)) / BigInt(100),
-      gasPrice: gasPrice,
-    });
+    let tx;
+    if (chainConfig.gasPredictable) {
+      modalInterface.waiting('CallStatic...');
+      await contract.callStatic['mintAndRegister'](product);
+      modalInterface.waiting('Estimating gas...');
+      const gasEstimation = (
+        await contract.estimateGas['mintAndRegister'](product)
+      ).toBigInt();
+      modalInterface.waiting('Got gas estimation: ' + gasEstimation);
+      const gasPrice = (await getGasPrice(chainConfig.provider)).valueOf();
+      modalInterface.waiting('Got gas price: ' + gasPrice);
+      modalInterface.waiting('Sending transaction...');
+      tx = await contract['mintAndRegister'](product, {
+        gasLimit: (gasEstimation * BigInt(105)) / BigInt(100),
+        gasPrice: gasPrice,
+      });
+    } else {
+      modalInterface.waiting('Sending transaction...');
+      tx = await contract['mintAndRegister'](product);
+    }
     await tx.wait();
+    modalInterface.success('Transaction successful');
     return { transactionHash: tx.hash };
   } catch (e: any) {
-    console.error(e);
-    if (e.code.toString() === 'ACTION_REJECTED') {
+    if (e.code && e.code.toString() === 'ACTION_REJECTED') {
       context.modalInterface.error('Transaction Rejected');
       throw new Error('Transaction Rejected');
     }
-    const err = contract.interface.parseError(e.data);
-    if (err.name === 'OwnableUnauthorizedAccount') {
-      context.modalInterface.error('You are not the owner of the shop');
-      throw new Unauthorized('record', chainConfig.address, shopAddress);
+    try {
+      const err = contract.interface.parseError(e.data);
+      if (err.name === 'OwnableUnauthorizedAccount') {
+        context.modalInterface.error('You are not the owner of the shop');
+        throw new Unauthorized(
+          'record',
+          chainConfig.address,
+          shopContractAddress
+        );
+      } else {
+        context.modalInterface.error(err.name);
+      }
+      context.modalInterface.error(e);
+      throw e;
+    } catch (error) {
+      modalInterface.error(`Unexpected error: ${error}`);
+      throw error;
     }
-    context.modalInterface.error(e);
-    throw e;
   }
 }
 
