8431f693a351a5d816fbb40c930ca767119ce34c
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-19 09:48:27 +0330

    Refactor: Deploy shop process for evm chains, Add more and better error handling for the process, enhance code

--- packages/web3/src/lib/chains/providers/evm/evmDeployShop.ts
+++ packages/web3/src/lib/chains/providers/evm/evmDeployShop.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ethers } from 'ethers';
-import { Chain, Network } from '../../dto/chains';
 import {
   ContractType,
   getDeployerAddress,
@@ -10,184 +9,202 @@ import {
 } from '../../dto/constants/chain-constants';
 import { deployerABI } from '../../dto/constants/chain-abis';
 import { chainLink } from '../../dto/configs/chainlink-addresses';
-import { ModalInterface } from '../../dto/interfaces/modal-interface.interface';
+import { DroplinkedChainConfig } from '../../dto/configs/chain.config';
+import { IWeb3Context } from '../../dto/interfaces/web3-context.interface';
+import { IDeployShop } from '../../dto/interfaces/deploy-shop.interface';
+import {
+  DeployShopResponse,
+  EthAddress,
+  UserDeniedException,
+} from '../../../web3';
+import { checkWallet } from './evm.helpers';
 
-export async function EVMDeployShop(
-  provider: any,
-  chain: Chain,
-  network: Network,
-  address: string,
-  shopName: string,
-  shopAddress: string,
-  shopOwner: string,
-  shopLogo: string,
-  shopDescription: string,
-  modalInterface: ModalInterface,
-  contractType: ContractType
+const sixify = (num: number): string => {
+  return num.toString().padStart(6, '0');
+};
+
+const deploymentParameters = {
+  [ContractType.TYPE0]: [
+    'string',
+    'string',
+    'address',
+    'string',
+    'string',
+    'address',
+    'address',
+    'address',
+  ],
+  [ContractType.TYPE1]: [
+    'string',
+    'string',
+    'address',
+    'string',
+    'string',
+    'address',
+    'address',
+  ],
+  [ContractType.TYPE2]: [],
+  [ContractType.TYPE3]: [
+    'string',
+    'string',
+    'address',
+    'string',
+    'string',
+    'address',
+  ],
+};
+
+async function getConstructorArgs(
+  contractType: ContractType,
+  shopDetails: IDeployShop,
+  deployerAddress: EthAddress,
+  chainConfig: DroplinkedChainConfig
 ) {
-  console.log({
-    chain,
-    network,
-    address,
-    shopName,
-    shopAddress,
-    shopOwner,
-    shopLogo,
-    shopDescription,
-  });
-  const signer = provider.getSigner();
-  modalInterface.waiting('got the signer: ' + (await signer.getAddress()));
-  if (
-    (await signer.getAddress()).toLocaleLowerCase() !==
-    address.toLocaleLowerCase()
-  ) {
-    throw new Error('Address does not match signer address');
+  const commonArgs = [
+    shopDetails.shopName || '',
+    shopDetails.shopAddress || '',
+    shopDetails.shopOwner,
+    shopDetails.shopLogo || '',
+    shopDetails.shopDescription || '',
+    deployerAddress,
+  ];
+
+  if (contractType === ContractType.TYPE0) {
+    return [
+      ...commonArgs,
+      (chainLink as any)[chainConfig.chain][chainConfig.network],
+      await getFundsProxy(chainConfig.chain, chainConfig.network),
+    ];
+  } else if (contractType === ContractType.TYPE1) {
+    return [
+      ...commonArgs,
+      (chainLink as any)[chainConfig.chain][chainConfig.network],
+    ];
+  } else if (contractType === ContractType.TYPE3) {
+    return commonArgs;
+  } else {
+    return [
+      ...commonArgs,
+      await getFundsProxy(chainConfig.chain, chainConfig.network),
+    ];
   }
+}
+
+export async function deployEVMShop(
+  chainConfig: DroplinkedChainConfig,
+  web3Context: IWeb3Context,
+  shopDetails: IDeployShop
+): Promise<DeployShopResponse> {
+  // console.log(ethers.utils.id('(address)'));
+
+  console.log({
+    ...chainConfig,
+    ...web3Context,
+    ...shopDetails,
+  });
+
+  const { modalInterface } = web3Context;
+  const { address, chain, contractType, network, provider } = chainConfig;
+  const signer = provider.getSigner();
+
+  modalInterface.waiting('got the signer: ' + (await signer.getAddress()));
+
+  await checkWallet(signer, address);
+
   const deployerAddress = await getDeployerAddress(chain, network);
-  modalInterface.waiting('Getting ready to deploy...');
+
+  modalInterface.waiting('Got deployer contract address');
+
   const contract = new ethers.Contract(deployerAddress, deployerABI, signer);
-  modalInterface.waiting('got deployer contract');
+
+  modalInterface.waiting('Getting shop bytecode');
+
   const byteCode = await getShopByteCode(contractType);
+
   modalInterface.waiting('got bytecode');
-  const sixify = (num: number): string => {
-    return num.toString().padStart(6, '0');
-  };
+
   const salt =
     '0x' +
     address.split('0x')[1] +
     '000000000000000000' +
     sixify(Math.floor(Math.random() * 1000000) + 1);
+
   modalInterface.waiting('created salt');
-  let constructorArgs;
-  if (chain === Chain.REDBELLY || chain === Chain.SKALE) {
-    console.log('Skale or redbelly');
-    constructorArgs = [
-      shopName ? shopName : '',
-      shopAddress,
-      shopOwner,
-      shopLogo ? shopLogo : '',
-      shopDescription ? shopDescription : '',
-      deployerAddress,
-    ];
-  } else
-    constructorArgs = [
-      shopName ? shopName : '',
-      shopAddress,
-      shopOwner,
-      shopLogo ? shopLogo : '',
-      shopDescription ? shopDescription : '',
-      deployerAddress,
-      (chainLink as any)[chain][network],
-      await getFundsProxy(chain, network),
-    ];
+  console.log(salt);
+
+  const constructorArgs = await getConstructorArgs(
+    contractType,
+    shopDetails,
+    deployerAddress,
+    chainConfig
+  );
+
   modalInterface.waiting('created constructor args');
-  let bytecodeWithArgs;
-  if (chain === Chain.REDBELLY || chain === Chain.SKALE) {
-    bytecodeWithArgs = ethers.utils.defaultAbiCoder.encode(
-      ['string', 'string', 'address', 'string', 'string', 'address'],
-      constructorArgs
-    );
-  } else {
-    bytecodeWithArgs = ethers.utils.defaultAbiCoder.encode(
-      [
-        'string',
-        'string',
-        'address',
-        'string',
-        'string',
-        'address',
-        'address',
-        'address',
-      ],
-      constructorArgs
-    );
-  }
+  console.log(constructorArgs);
+
+  const bytecodeWithArgs = ethers.utils.defaultAbiCoder.encode(
+    deploymentParameters[contractType],
+    constructorArgs
+  );
+
   modalInterface.waiting('Created bytecodeWithArgs');
+  console.log(bytecodeWithArgs);
+
   try {
-    if (chain !== Chain.REDBELLY && chain !== Chain.SKALE) {
-      modalInterface.waiting('Deploying');
-      await contract.callStatic['deployShop'](
-        byteCode + bytecodeWithArgs.split('0x')[1],
-        salt
-      );
+    let tx: any;
+    const fullBytecode = byteCode + bytecodeWithArgs.split('0x')[1];
+    if (chainConfig.gasPredictable) {
+      modalInterface.waiting('CallStatic to estimate gas');
+      await contract.callStatic['deployShop'](fullBytecode, salt);
+      modalInterface.waiting('Estimating gas');
       const gasEstimation = (
-        await contract.estimateGas['deployShop'](
-          byteCode + bytecodeWithArgs.split('0x')[1],
-          salt
-        )
+        await contract.estimateGas['deployShop'](fullBytecode, salt)
       )
         .toBigInt()
         .valueOf();
-      modalInterface.waiting('Deploying Shop...');
-      const tx = await contract['deployShop'](
-        byteCode + bytecodeWithArgs.split('0x')[1],
-        salt,
-        {
-          gasLimit: (gasEstimation * BigInt(105)) / BigInt(100),
-          gasPrice: getGasPrice(provider),
-        }
-      );
-      modalInterface.waiting('Waiting for confirmation...');
-      const receipt = await tx.wait();
-      const logs = receipt.logs
-        .map((log: any) => {
-          try {
-            return contract.interface.parseLog(log);
-          } catch {
-            return null;
-          }
-        })
-        .filter((log: any) => log != null);
-      const shopDeployLog = logs.find(
-        (log: any) => log.name === 'ShopDeployed'
-      );
-      const deployedShopAddress = shopDeployLog.args.shop;
-      const deployedNFTAddress = shopDeployLog.args.nftContract;
-      modalInterface.success('Shop Deployed');
-      return {
-        transaction_id: tx.hash,
-        deployedShopAddress,
-        deployedNFTAddress,
-      };
+      modalInterface.waiting('Sending transaction');
+      tx = await contract['deployShop'](fullBytecode, salt, {
+        gasLimit: (gasEstimation * BigInt(105)) / BigInt(100),
+        gasPrice: await getGasPrice(provider),
+      });
     } else {
-      modalInterface.waiting('Deploying Shop...');
-
-      const tx = await contract['deployShop'](
-        byteCode + bytecodeWithArgs.split('0x')[1],
-        salt
-      );
-      modalInterface.waiting('Waiting for confirmation...');
-      const receipt = await tx.wait();
-      const logs = receipt.logs
-        .map((log: any) => {
-          try {
-            return contract.interface.parseLog(log);
-          } catch (e: any) {
-            console.log(`failed to parse logs: ${e}`);
-            return null;
-          }
-        })
-        .filter((log: any) => log !== null) as any;
-      const shopDeployLog = logs.find(
-        (log: any) => log.name === 'ShopDeployed'
-      );
-      const deployedShopAddress = shopDeployLog.args.shop;
-      const deployedNFTAddress = shopDeployLog.args.nftContract;
-      modalInterface.success('Shop Deployed');
-      return {
-        transaction_id: tx.hash,
-        deployedShopAddress,
-        deployedNFTAddress,
-      };
+      modalInterface.waiting('Sending transaction');
+      tx = await contract['deployShop'](fullBytecode, salt);
     }
+    modalInterface.waiting('Waiting for confirmation...');
+    const receipt = await tx.wait();
+    modalInterface.waiting('Transaction confirmed, extracting data...');
+    const logs = receipt.logs
+      .map((log: any) => {
+        try {
+          return contract.interface.parseLog(log);
+        } catch {
+          return null;
+        }
+      })
+      .filter((log: any) => log != null);
+    const shopDeployLog = logs.find((log: any) => log.name === 'ShopDeployed');
+    const deployedShopAddress = shopDeployLog.args.shop;
+    const deployedNFTAddress = shopDeployLog.args.nftContract;
+    modalInterface.success('Deployment successful!');
+    return {
+      transactionHash: tx.hash,
+      deployedShopAddress,
+      deployedNFTAddress,
+    };
   } catch (e: any) {
-    console.log(e);
+    modalInterface.error('Error during deployment.');
     if (e.code.toString() === 'ACTION_REJECTED') {
       modalInterface.error('Transaction Rejected');
-      throw new Error('Transaction Rejected');
+      throw new UserDeniedException('ShopDeployment Rejected by user');
     }
-    const err = contract.interface.parseError(e.data);
-    modalInterface.error(err as any);
-    throw err;
+    try {
+      const err = contract.interface.parseError(e.data);
+      modalInterface.error(`Contract error: ${err}`);
+      throw err;
+    } catch (parseError: any) {
+      modalInterface.error('Unhandled error during deployment.');
+      throw new Error(parseError || 'Unknown error occurred.');
+    }
   }
 }
