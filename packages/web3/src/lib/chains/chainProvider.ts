ade338ad1b0163196633bc7b9a577d31c5b79547
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-19 09:41:49 +0330

    Refactor + Add second form of wallet login

--- packages/web3/src/lib/chains/chainProvider.ts
+++ packages/web3/src/lib/chains/chainProvider.ts
@@ -1,139 +1,249 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Chain, Network } from './dto/chains';
 import { EVMProvider } from './providers/evm/evmProvider';
 import { ContractType } from './dto/constants/chain-constants';
-import { ChainNotImplementedException, defaultModal } from '../web3';
-import { Web3Config } from './dto/configs/web3-config';
+import {
+  AccountAccessDeniedException,
+  ChainNotImplementedException,
+  defaultModal,
+  MetaMaskNotFoundException,
+  NoAccountsFoundException,
+  SignatureRequestDeniedException,
+  WalletError,
+  WalletNotFoundException,
+} from '../web3';
+import { Web3ChainConfig } from './dto/configs/web3-config';
 import { IChainProvider } from './dto/interfaces/chain-provider.interface';
+import { AxiosInstance } from 'axios';
+import { getAccounts } from './providers/evm/evmLogin';
+import { ethers } from 'ethers';
+export class DropWeb3 {
+  private axiosInstance: AxiosInstance;
+  constructor(axiosInstance: AxiosInstance) {
+    this.axiosInstance = axiosInstance;
+  }
+  private chainMapping = {
+    [Chain.BINANCE]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.BINANCE,
+        Network.TESTNET,
+        ContractType.TYPE0,
+        true
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.BINANCE,
+        Network.MAINNET,
+        ContractType.TYPE0,
+        true
+      ),
+    },
+    [Chain.POLYGON]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.POLYGON,
+        Network.TESTNET,
+        ContractType.TYPE1,
+        true
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.POLYGON,
+        Network.MAINNET,
+        ContractType.TYPE0,
+        true
+      ),
+    },
+    [Chain.NEAR]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.NEAR,
+        Network.TESTNET,
+        ContractType.TYPE3,
+        true // todo
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.NEAR,
+        Network.MAINNET,
+        ContractType.TYPE3,
+        true //todo
+      ),
+    },
+    [Chain.CASPER]: {
+      [Network.TESTNET]: null,
+      [Network.MAINNET]: null,
+    },
+    [Chain.XRPLSIDECHAIN]: {
+      [Network.TESTNET]: null,
+      [Network.MAINNET]: null,
+    },
+    [Chain.BASE]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.BASE,
+        Network.TESTNET,
+        ContractType.TYPE1,
+        true
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.BASE,
+        Network.MAINNET,
+        ContractType.TYPE0,
+        true
+      ),
+    },
+    [Chain.STACKS]: {
+      [Network.TESTNET]: null,
+      [Network.MAINNET]: null,
+    },
+    [Chain.SKALE]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.SKALE,
+        Network.TESTNET,
+        ContractType.TYPE3,
+        false
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.SKALE,
+        Network.MAINNET,
+        ContractType.TYPE3,
+        false
+      ),
+    },
+    [Chain.LINEA]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.LINEA,
+        Network.TESTNET,
+        ContractType.TYPE3,
+        true
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.LINEA,
+        Network.MAINNET,
+        ContractType.TYPE0,
+        true
+      ),
+    },
+    [Chain.ETH]: {
+      [Network.TESTNET]: new EVMProvider(
+        Chain.ETH,
+        Network.TESTNET,
+        ContractType.TYPE1,
+        true
+      ),
+      [Network.MAINNET]: new EVMProvider(
+        Chain.ETH,
+        Network.MAINNET,
+        ContractType.TYPE0,
+        true
+      ),
+    },
+    [Chain.SOLANA]: {
+      [Network.MAINNET]: null,
+      [Network.TESTNET]: null,
+    },
+    [Chain.REDBELLY]: {
+      [Network.MAINNET]: new EVMProvider(
+        Chain.REDBELLY,
+        Network.MAINNET,
+        ContractType.TYPE3,
+        false
+      ),
+      [Network.TESTNET]: new EVMProvider(
+        Chain.REDBELLY,
+        Network.TESTNET,
+        ContractType.TYPE3,
+        false
+      ),
+    },
+  };
 
-const chainMapping = {
-  [Chain.BINANCE]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.BINANCE,
-      Network.TESTNET,
-      ContractType.TYPE0
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.BINANCE,
-      Network.MAINNET,
-      ContractType.TYPE0
-    ),
-  },
-  [Chain.POLYGON]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.POLYGON,
-      Network.TESTNET,
-      ContractType.TYPE1
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.POLYGON,
-      Network.MAINNET,
-      ContractType.TYPE0
-    ),
-  },
-  [Chain.NEAR]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.NEAR,
-      Network.TESTNET,
-      ContractType.TYPE3
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.NEAR,
-      Network.MAINNET,
-      ContractType.TYPE3
-    ),
-  },
-  [Chain.CASPER]: {
-    [Network.TESTNET]: null,
-    [Network.MAINNET]: null,
-  },
-  [Chain.XRPLSIDECHAIN]: {
-    [Network.TESTNET]: null,
-    [Network.MAINNET]: null,
-  },
-  [Chain.BASE]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.BASE,
-      Network.TESTNET,
-      ContractType.TYPE1
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.BASE,
-      Network.MAINNET,
-      ContractType.TYPE0
-    ),
-  },
-  [Chain.STACKS]: {
-    [Network.TESTNET]: null,
-    [Network.MAINNET]: null,
-  },
-  [Chain.SKALE]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.SKALE,
-      Network.TESTNET,
-      ContractType.TYPE3
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.SKALE,
-      Network.MAINNET,
-      ContractType.TYPE3
-    ),
-  },
-  [Chain.LINEA]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.LINEA,
-      Network.TESTNET,
-      ContractType.TYPE3
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.LINEA,
-      Network.MAINNET,
-      ContractType.TYPE0
-    ),
-  },
-  [Chain.ETH]: {
-    [Network.TESTNET]: new EVMProvider(
-      Chain.ETH,
-      Network.TESTNET,
-      ContractType.TYPE1
-    ),
-    [Network.MAINNET]: new EVMProvider(
-      Chain.ETH,
-      Network.MAINNET,
-      ContractType.TYPE0
-    ),
-  },
-  [Chain.SOLANA]: {
-    [Network.MAINNET]: null,
-    [Network.TESTNET]: null,
-  },
-  [Chain.REDBELLY]: {
-    [Network.MAINNET]: new EVMProvider(
-      Chain.REDBELLY,
-      Network.MAINNET,
-      ContractType.TYPE3
-    ),
-    [Network.TESTNET]: new EVMProvider(
-      Chain.REDBELLY,
-      Network.TESTNET,
-      ContractType.TYPE3
-    ),
-  },
-};
+  web3Instance(config: Web3ChainConfig): IChainProvider {
+    const { chain, network, modalInterface, preferredWallet, userAddress } =
+      config;
 
-export function Web3Instance(config: Web3Config): IChainProvider {
-  const { chain, network, modalInterface, preferredWallet, userAddress } =
-    config;
+    if (this.chainMapping[chain][network] == null)
+      throw new ChainNotImplementedException(
+        `The given chain <${chain}> and network <${network}> is not implemented yet`
+      );
 
-  if (chainMapping[chain][network] == null)
-    throw new ChainNotImplementedException(
-      `The given chain <${chain}> and network <${network}> is not implemented yet`
-    );
+    return this.chainMapping[chain][network]
+      ?.setAddress(userAddress || '')
+      .setModal(modalInterface || new defaultModal())
+      .setWallet(preferredWallet)
+      .setAxiosInstance(this.axiosInstance)
+      .setNFTContractAddress(config.nftContractAddress || '')
+      .setShopContractAddress(config.shopContractAddress || '');
+  }
 
-  return chainMapping[chain][network]
-    ?.setAddress(userAddress || '')
-    .setModal(modalInterface || new defaultModal())
-    .setWallet(preferredWallet)
-    .setAxiosInstance(config.axiosInstance)
-    .setNFTContractAddress(config.nftContractAddress || '')
-    .setShopContractAddress(config.shopContractAddress || '');
+  async getWalletInfo() {
+    try {
+      const ethereum = (window as any).ethereum;
+
+      // Check if an Ethereum provider is available
+      if (!ethereum) {
+        throw new WalletNotFoundException('No EVM Wallet is installed');
+      }
+
+      // Check if MetaMask is installed
+      if (!ethereum.isMetaMask) {
+        throw new MetaMaskNotFoundException();
+      }
+
+      // Request accounts access
+      try {
+        await ethereum.request({ method: 'eth_requestAccounts' });
+      } catch (error: any) {
+        // Handle case when user denies account access
+        throw new AccountAccessDeniedException(error.message);
+      }
+
+      const nonce = Math.floor(Math.random() * 10000000);
+
+      // Get the current date and time for transparency
+      const currentDate = new Date().toLocaleString();
+
+      const message = `Welcome to Droplinked!
+      
+      Please sign this message to verify your identity and securely log in.
+      
+      - Nonce: ${nonce}
+      - Date: ${currentDate}
+      
+      This action will not incur any gas fees or blockchain transactions.`;
+
+      // Get the account address
+      let address;
+      try {
+        const accounts = await getAccounts(ethereum);
+        if (!accounts || accounts.length === 0) {
+          throw new NoAccountsFoundException();
+        }
+        address = accounts[0];
+      } catch (error: any) {
+        // Handle error in retrieving accounts
+        throw new WalletError(
+          `Failed to retrieve account address: ${error.message}`
+        );
+      }
+
+      // Request the user to sign the message
+      let signature;
+      try {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        signature = await signer.signMessage(message);
+      } catch (error: any) {
+        // Handle case where the user refuses to sign the message
+        throw new SignatureRequestDeniedException(error.message);
+      }
+
+      // Return the wallet information
+      return {
+        address: address,
+        signature: signature,
+        nonce: nonce,
+        date: currentDate,
+      };
+    } catch (error: any) {
+      // General catch for unexpected errors
+      console.error('Error fetching wallet information:', error.message);
+      throw error; // Re-throw the error to let the caller handle it
+    }
+  }
 }
