58c816d84ab427e3d8505aac23d808f1cfb0d997
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-20 23:07:24 +0330

    Update the droplinked testing project to reflect changes made

--- app/droplinked/src/app/app.tsx
+++ app/droplinked/src/app/app.tsx
@@ -14,6 +14,7 @@ import { FC } from 'react';
 
 interface IDeployParams {
   chainName: Chain;
+  address: string;
 }
 
 interface IRecordProduct {
@@ -27,23 +28,19 @@ export const DeployShop: FC<IDeployParams> = (deployParams: IDeployParams) => {
   return (
     <button
       onClick={async () => {
-        const web3 = new DropWeb3(
-          axios.create({
-            baseURL: 'https://apiv3dev.droplinked.com',
-          })
-        );
+        const web3 = new DropWeb3(Network.TESTNET);
+
         const binance = web3.web3Instance({
+          method: 'deploy',
           chain: deployParams.chainName,
-          network: Network.TESTNET,
           preferredWallet: ChainWallet.Metamask,
-          userAddress: '0x734ce112E36B915a688D803FD9C57F339cBe410b',
+          userAddress: deployParams.address,
         });
         const result = await binance.deployShop({
           shopAddress: 'https://k3rn3lpanicc.com',
           shopDescription: 'lol',
           shopLogo: 'na',
           shopName: 'k3rn3lpanic',
-          shopOwner: '0x734ce112E36B915a688D803FD9C57F339cBe410b',
         });
         console.log({ result });
       }}
@@ -59,36 +56,32 @@ export const RecordProduct: FC<IRecordProduct> = (
   return (
     <button
       onClick={async () => {
-        const web3 = new DropWeb3(
-          axios.create({
-            baseURL: 'https://apiv3dev.droplinked.com',
-          })
-        );
-        const binance = web3.web3Instance({
+        const web3 = new DropWeb3(Network.TESTNET);
+        const chainInstance = web3.web3Instance({
+          method: 'record-affiliate',
           chain: recordParams.chainName,
-          network: Network.TESTNET,
           preferredWallet: ChainWallet.Metamask,
           userAddress: recordParams.walletAddress,
           nftContractAddress: recordParams.nftContract,
           shopContractAddress: recordParams.shopAddress,
         });
-        const result = await binance.recordProduct(
+        const result = await chainInstance.recordProduct(
           {
-            acceptsManageWallet: true,
-            commission: 0,
-            currencyAddress: '0x0000000000000000000000000000000000000000',
-            description: 'desc',
             productTitle: 'title',
+            description: 'desc',
             royalty: 0,
-            skuProperties: {},
+            commission: 0,
             type: ProductType.DIGITAL,
+            acceptsManageWallet: true,
+            currencyAddress: '0x0000000000000000000000000000000000000000',
           },
           {
             amount: 1000,
-            beneficiaries: [],
-            imageUrl: 'https://k3rn3lpanic.img',
             price: 1000,
+            beneficiaries: [],
+            skuProperties: {},
             skuID: '670fcb2ea6738730848f8933',
+            imageUrl: 'https://k3rn3lpanic.img',
           }
         );
         console.log({ result });
@@ -104,14 +97,10 @@ export function App() {
     <div>
       <button
         onClick={async () => {
-          const web3 = new DropWeb3(
-            axios.create({
-              baseURL: 'https://apiv3dev.droplinked.com',
-            })
-          );
+          const web3 = new DropWeb3(Network.TESTNET);
           const chainProvider = web3.web3Instance({
+            method: 'login',
             chain: Chain.BASE,
-            network: Network.TESTNET,
             preferredWallet: ChainWallet.Metamask,
           });
           const loginData = await chainProvider.walletLogin();
@@ -124,11 +113,8 @@ export function App() {
       <br></br>
       <button
         onClick={async () => {
-          const web3 = new DropWeb3(
-            axios.create({
-              baseURL: 'https://apiv3dev.droplinked.com',
-            })
-          );
+          const web3 = new DropWeb3(Network.TESTNET);
+
           const info = await web3.getWalletInfo();
           console.log({ info });
         }}
@@ -137,21 +123,43 @@ export function App() {
       </button>
       <br></br>
       <br></br>
-      <DeployShop chainName={Chain.BINANCE} />
+      <DeployShop
+        chainName={Chain.BINANCE}
+        address="0xe29E7479c23Db494aAa0D36C93844B2d79f50c25"
+      />
       <br></br>
-      <DeployShop chainName={Chain.SKALE} />
+      <DeployShop
+        chainName={Chain.SKALE}
+        address="0xe29E7479c23Db494aAa0D36C93844B2d79f50c25"
+      />
       <br></br>
-      <DeployShop chainName={Chain.POLYGON} />
+      <DeployShop
+        chainName={Chain.POLYGON}
+        address="0x9CA686090b4c6892Bd76200e3fAA2EeC98f0528F"
+      />
       <br></br>
-      <DeployShop chainName={Chain.BASE} />
+      <DeployShop
+        chainName={Chain.BASE}
+        address="0xBEc8C184A8f55E6443B315361Bac3BbB2280E8E8"
+      />
       <br></br>
-      <DeployShop chainName={Chain.REDBELLY} />
+      <DeployShop
+        chainName={Chain.REDBELLY}
+        address="0x734ce112E36B915a688D803FD9C57F339cBe410b"
+      />
       <br></br>
       <RecordProduct
         chainName={Chain.REDBELLY}
         nftContract="0xEEBB0Ee8779e58c4ba881EB3BB2BEAd81ea3f119"
         shopAddress="0x24780563F3b2a498D162ce420fE7788311517F0B"
         walletAddress="0x734ce112E36B915a688D803FD9C57F339cBe410b"
+      />
+      <br></br>
+      <RecordProduct
+        chainName={Chain.POLYGON}
+        nftContract="0x1C0b995351A7701692c634Bc8a3Ba8B3b10BCCBE"
+        shopAddress="0xD8edb8198e7eF42790Af2e43339c8454d10fA7CB"
+        walletAddress="0x9CA686090b4c6892Bd76200e3fAA2EeC98f0528F"
       />
     </div>
   );
