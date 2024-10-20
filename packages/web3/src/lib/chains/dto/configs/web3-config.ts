bcc1444cb18f054cce312d29cae29b5968e7ca87
Mohammad matin <matin.ghiasvand1381@gmail.com>
2024-10-20 23:04:31 +0330

    Update web3config: add method and convert the type into polymorph type

--- packages/web3/src/lib/chains/dto/configs/web3-config.ts
+++ packages/web3/src/lib/chains/dto/configs/web3-config.ts
@@ -1,12 +1,32 @@
-import { Chain, ChainWallet, Network } from '../chains';
+import { Chain, ChainWallet } from '../chains';
 import { ModalInterface } from '../interfaces/modal-interface.interface';
 
-export type Web3ChainConfig = {
-  chain: Chain;
-  network: Network;
-  preferredWallet: ChainWallet;
-  modalInterface?: ModalInterface;
-  userAddress?: string;
-  nftContractAddress?: string;
-  shopContractAddress?: string;
-};
+export type Web3ChainConfig =
+  | {
+      method: 'login';
+      chain: Chain;
+      preferredWallet: ChainWallet;
+      modalInterface?: ModalInterface;
+    }
+  | {
+      method: 'deploy';
+      chain: Chain;
+      preferredWallet: ChainWallet;
+      modalInterface?: ModalInterface;
+      userAddress: string;
+    }
+  | {
+      method: 'record-affiliate';
+      chain: Chain;
+      preferredWallet: ChainWallet;
+      modalInterface?: ModalInterface;
+      userAddress: string;
+      nftContractAddress: string;
+      shopContractAddress: string;
+    }
+  | {
+      method: 'payment';
+      chain: Chain;
+      preferredWallet: ChainWallet;
+      modalInterface?: ModalInterface;
+    };
