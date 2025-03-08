b62cdf1b91161add0e446f490dc829992b58bc79
bedimns <bedi.mns@gmail.com>
2025-03-08 14:43:25 +0330

    remoev enum

--- app/droplinked/src/app/app.tsx
+++ app/droplinked/src/app/app.tsx
@@ -1,9 +1,49 @@
-import {
-  DroplinkedPaymentIntent,
-  PaymentType,
-} from 'droplinked-payment-intent';
+import { DroplinkedPaymentIntent,  Appearance } from 'droplinked-payment-intent';
+
 function app() {
-  return <DroplinkedPaymentIntent type={PaymentType.Stripe} clientSecret="pi_3R0JysJYpy7bkFtu0zIpAWCr_secret_gMDigNJaZk7I9BRCh2fnrh6eD"  />;
+  const appearance: Appearance = {
+    theme: 'dark',
+    labels: 'above',
+    variables: {
+      colorBackground: '#1a1a1a',
+      colorBackgroundText: '#ffffff',
+      colorText: '#ffffff',
+      borderRadius: '8px',
+      colorSuccess: '#2BCFA1',
+      colorDanger: '#FF2244',
+      focusOutline: 'unset',
+      focusBoxShadow: 'none',
+    },
+    rules: {
+      '.Input:focus': {
+        border: '1px solid #7B7B7B',
+      },
+      '.Input:hover': {
+        border: '1px solid #3C3C3C',
+      },
+      '.Input': {
+        border: '1px solid #292929',
+        borderRadius: '8px',
+        padding: '10px',
+        marginBottom: '12px',
+      },
+      '.Label': {
+        marginBottom: '12px',
+        fontWeight: '500',
+      },
+    }
+  };
+
+  return (
+    <DroplinkedPaymentIntent 
+      clientSecret="pi_3R0LESJYpy7bkFtu1JA4ZrYM_secret_JTYYYectX3YhsNUmTJGVJKgIe"
+      type='stripe'
+      appearance={appearance}
+      return_url="droplinked.com"
+      onSuccess={() => console.log('پرداخت با موفقیت انجام شد')}
+      onError={(error) => console.error('خطا در پرداخت:', error)}
+    />
+  );
 }
 
 export default app;
--- packages/droplinked-payment-intent/src/index.ts
+++ packages/droplinked-payment-intent/src/index.ts
@@ -1,2 +1,2 @@
 export * from './lib/droplinked-payment-intent';
-export * from './lib/enums';
+
